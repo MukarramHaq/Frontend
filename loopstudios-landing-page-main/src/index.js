0509bf73cd09b0223161a37dd3dbbfd2a7056c04
MukarramHaq <mukarram796@gmail.com>
2025-01-28 18:00:41 -0500

    feat:desktop version done

--- loopstudios-landing-page-main/src/index.js
+++ loopstudios-landing-page-main/src/index.js
@@ -2,8 +2,11 @@ const hamBtnEl = document.querySelector('.ham-btn');
 const closeBtnEl = document.getElementById('close-btn');
 const sidebarEl = document.getElementById('sidebar');
 
+console.log(hamBtnEl);
+
 hamBtnEl.addEventListener('click', () => {
     sidebarEl.style.display = 'block';
+    console.log('pressed');
 })
 
 closeBtnEl.addEventListener('click', () => {
